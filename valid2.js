e6b0a195a977002a3714765359cefdf098b17e16
alyssatangonan <60341374+alyssatangonan@users.noreply.github.com>
2020-02-20 00:40:00 +0800

    Update valid2.js

--- valid2.js
+++ valid2.js
@@ -2,58 +2,51 @@ function compare()
 {
         var pass = document.getElementById('pass').value;
         var pass2 = document.getElementById('pass2').value;
-        
-        if (pass2 !== pass)
-        {
-                document.getElementById('pass').innerHTML = 'Passwords are not the same';
-        }
-        else
-        {
-                document.getElementById('pass').innerHTML = 'Passwords are same';
+
+        if (pass2==""){
+            document.getElementById("passmsg").innerHTML = '';
+
+        }else{
+            if (pass2 !== pass){
+                document.getElementById("passmsg").innerHTML = 'Passwords are not the same';
+            } else if (pass2===pass){
+                document.getElementById("passmsg").innerHTML = 'Passwords are same';
+
+            }
         }
 }
-/*
-function validate(pass)
-{
+
+function checkemail(){
+    var email = document.getElementById('email').value;
+    if (email==""){
+        document.getElementById("emailmsg").innerHTML = '';
+
+    }else{
+         if (email.includes("@")==true){
+             document.getElementById("emailmsg").innerHTML = 'Mail is correct';
+            } else {
+             document.getElementById("emailmsg").innerHTML = 'Mail is wrong';
+            }
+        }
+
+}
+
+function checkpass(){
     var pass = document.getElementById('pass').value;
-        var pass2 = document.getElementById('pass2').value;
-    if (pass.length === 0) {
-        document.getElementById("pass").innerHTML = "";
-        return;
-    } 
-    // Create an array and push all possible values that you want in password
-    var matchedCase = new Array();
-    matchedCase.push("[$@$!%*#?&]"); // Special Charector
-    matchedCase.push("[A-Z]");      // Uppercase Alpabates
-    matchedCase.push("[0-9]");      // Numbers
-    matchedCase.push("[a-z]");     // Lowercase Alphabates
+    if (pass==""){
+        document.getElementById("checkmsg").innerHTML = '';
 
-    // Check the conditions
-    var ctr = 0;
-    for (var i = 0; i < matchedCase.length; i++) {
-        if (new RegExp(matchedCase[i]).test(pass)) {
-            ctr++;
+    }else{
+         if ((pass.includes("@")==true)&&(pass>="A")&&(pass>="a")){
+             document.getElementById("checkmsg").innerHTML = 'Strong';
+            } else {
+             document.getElementById("checkmsg").innerHTML = 'Weak';
+            }
         }
-    }
-    // Display it
-    var strength = "";
-    switch (ctr) {
-        case 0:
-        case 1:
-        case 2:
-            strength = "Very Weak";
-            break;
-        case 3:
-            strength = "Medium";
-            break;
-        case 4:
-            strength = "Strong";
-            break;
-    }
-    document.getElementById("msg").innerHTML = strength;
+
+
+
 
 }
-
-
 
 
